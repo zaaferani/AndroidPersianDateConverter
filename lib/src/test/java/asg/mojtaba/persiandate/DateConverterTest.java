3bd8afc10aeb204378adaca22d0f5dd42f0fd2e0
Mojtaba Asg <asg.mojtaba@gmail.com>
2015-08-18 12:25:31 +0430

    add date convertor

--- lib/src/test/java/asg/mojtaba/persiandate/DateConverterTest.java
+++ lib/src/test/java/asg/mojtaba/persiandate/DateConverterTest.java
@@ -2,7 +2,7 @@ package asg.mojtaba.persiandate;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Mojtaba Asgari
@@ -15,5 +15,14 @@ public class DateConverterTest {
         DateConverter dateConverter = new DateConverter();
         SimpleDate simpleDate = dateConverter.gregorianToPersian(new SimpleDate(2015, 8, 17));
         SimpleDate gSimpleDate = dateConverter.persianToGregorian(new SimpleDate(1394, 5, 26));
+        assertEquals(simpleDate.year, 1394);
+        assertEquals(simpleDate.month, 5);
+        assertEquals(simpleDate.day, 26);
+
+
+        assertEquals(gSimpleDate.year, 2015);
+        assertEquals(gSimpleDate.month, 8);
+        assertEquals(gSimpleDate.day, 17);
+
     }
 }
